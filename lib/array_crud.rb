356c621657ce9b5ef7d06c4ed75e27806440c6ff
Jorge Ramirez <joramirez96@gmail.com>
2020-03-25 16:47:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-   [Jorge Bryan Alberto Yaneirys]
+   ["Jorge", "Bryan", "Alberto", "Yaneirys"]
 end
 
 def add_element_to_end_of_array(array, element)
