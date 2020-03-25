81f979abfbd8388127d4ac8075d145270e223e36
Jorge Ramirez <joramirez96@gmail.com>
2020-03-25 16:41:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,15 +3,15 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my_array = [Jorge Bryan Alberto Yaneirys]
+   [Jorge Bryan Alberto Yaneirys]
 end
 
 def add_element_to_end_of_array(array, element)
-
+  my_array << Tiffany
 end
 
 def add_element_to_start_of_array(array, element)
-
+  
 end
 
 def remove_element_from_end_of_array(array)
