6185847a7fcb359e2ea968c19e53343036d05041
Jorge Ramirez <joramirez96@gmail.com>
2020-03-25 16:36:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,12 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  <<
+  my_array << "Tiffany"
+  puts my_array.inspect 
 end
 
 def add_element_to_start_of_array(array, element)
-
+  
 end
 
 def remove_element_from_end_of_array(array)
